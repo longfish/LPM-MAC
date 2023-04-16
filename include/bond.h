a2e5d29bed238cc5ae72aedc664e4a8153d50d45
longfish <thomasmeng95@gmail.com>
2023-04-16 02:45:29 -0700

    consider broken bonds

--- include/bond.h
+++ include/bond.h
@@ -16,26 +16,30 @@ protected:
     static int _ID;
 
 public:
-    int id{0};                                    // identifier of the bond, id starts from 0
-    int layer{-1};                                // index of current bond layer
-    double dis_initial{0}, dis_last{0}, dis{0};   // initial, last, and current bond length
-    double ddL{0};                                // incremental change of particle distance
-    double dLe{0}, dLp{0}, dLp_last{0};           // elastic and plastic bond change
-    double Kn{0}, Tv{0};                          // LPM coefficient
-    double csx{0}, csy{0}, csz{0};                // direction cosine
-    double bforce_last{0}, bforce{0}, bdamage{0}; // bond-wise quantities
-    double d_indicator{0};                        // damage indicator
-    Particle<nlayer> *p1, *p2;                    // particles are not owned by the bond (only store the location)
+    int id{0};                                                     // identifier of the bond, id starts from 0
+    int layer{-1};                                                 // index of current bond layer
+    double dis_initial{0}, dis_last{0}, dis{0};                    // initial, last, and current bond length
+    double dL{0}, dLe{0}, dLp{0}, dLp_last{0};                     // total, elastic and plastic bond change
+    double Kn{0}, Tv{0};                                           // LPM coefficient
+    double csx{0}, csy{0}, csz{0};                                 // direction cosine
+    double bforce_last{0}, bforce{0}, bdamage{0}, bdamage_last{0}; // bond-wise quantities
+    Particle<nlayer> *p1, *p2;                                     // particles are not owned by the bond (only store the location)
 
-    void updatebGeometry()
+    bool updatebGeometry()
     {
-        dis_last = dis;
         dis = p1->distanceTo(p2);
+        dL = dis - dis_initial;
         dLe = dis - dis_initial - dLp;
-        ddL = dis - dis_last;
         csx = (p1->xyz[0] - p2->xyz[0]) / dis;
         csy = (p1->xyz[1] - p2->xyz[1]) / dis;
         csz = (p1->xyz[2] - p2->xyz[2]) / dis;
+        if (abs(bdamage - 1.0) < EPS)
+        {
+            dLe = 0, csx = 0, csy = 0, csz = 0;
+            return true; // true means the bond has broken
+        }
+
+        return false;
     }
 
     Bond(Particle<nlayer> *p_p1, Particle<nlayer> *p_p2)
@@ -45,9 +49,10 @@ public:
         p2 = p_p2;
         dis = p1->distanceTo(p2);
         dis_initial = dis;
-        if ((dis < 1.01 * p1->cell.neighbor1_cutoff) && (p1->id != p2->id))
+        dis_last = dis;
+        if ((dis < 1.01 * p1->cell.neighbor_cutoff[0]) && (p1->id != p2->id))
             layer = 0;
-        else if ((dis > 1.01 * p1->cell.neighbor1_cutoff) && (dis < 1.01 * p1->cell.neighbor2_cutoff))
+        else if ((dis > 1.01 * p1->cell.neighbor_cutoff[0]) && (dis < 1.01 * p1->cell.neighbor_cutoff[1]))
             layer = 1;
 
         csx = (p1->xyz[0] - p2->xyz[0]) / dis;
@@ -62,6 +67,7 @@ public:
         p2 = p_p2;
         dis = p_dis;
         dis_initial = p_dis;
+        dis_last = dis;
         layer = p_layer;
 
         csx = (p1->xyz[0] - p2->xyz[0]) / dis;
