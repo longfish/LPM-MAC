617a3c87fbc608489efa21d71e5f99e6be0699c6
longfish <thomasmeng95@gmail.com>
2023-06-05 16:55:53 -0700

    added more test cases

--- src/lpmcpp.cpp
+++ src/lpmcpp.cpp
@@ -14,12 +14,18 @@
 
 // #include "geo1_CT_configuration.cpp"
 // #include "geo2_dogbone_configuration.cpp"
+// #include "geo2_dogbone_config2d.cpp"
 
+#include "ex1_isoelasticity_2d_hex.cpp"
 // #include "ex1_isoelasticity_3d_sc.cpp"
 // #include "ex2_polycrystal_3d_bcc.cpp"
 // #include "ex3_singlecrystal_3d_fcc.cpp"
-#include "ex4_isoelasticity_2d_crack.cpp"
+// #include "ex4_isoelasticity_2d_crack.cpp"
+// #include "ex4_isoelasticity_2d_crack_shear.cpp"
+// #include "ex5_isoelasticity_2d_fatigue_gage.cpp"
+// #include "ex5_isoelasticity_2d_fatigue.cpp"
 // #include "ex5_isoelasticity_3d_fatigue.cpp"
+// #include "ex6_isoelasticity_2d_CT_fatigue_calib.cpp"
 // #include "ex6_isoelasticity_2d_CT_fatigue.cpp"
 
 /************************************************************************/
