7629fe82d0034965988cd8b2c36de6fd481fd6d2
longfish <thomasmeng95@gmail.com>
2023-06-05 16:52:56 -0700

    added CT and dogbone loading step

--- include/load_step_fatigue.h
+++ include/load_step_fatigue.h
@@ -12,7 +12,9 @@
 
 enum class FatigueLoadType : char
 {
-    LoadUniaxialDisp
+    LoadUniaxialDisp,
+    LoadDogBoneDisp,
+    LoadCTForce
 };
 
 // create the template for fatigue loading
@@ -20,7 +22,24 @@ template <int nlayer>
 class LoadUniaxialDisp : public LoadStep<nlayer>
 {
 public:
-    LoadUniaxialDisp(double x, double y, double z, std::vector<Particle<nlayer> *> &group1, std::vector<Particle<nlayer> *> &group2)
+    LoadUniaxialDisp(double dy,
+                    std::vector<Particle<nlayer> *> &left_group,
+                    std::vector<Particle<nlayer> *> &top_group,
+                    std::vector<Particle<nlayer> *> &bottom_group)
+    {
+        this->dispBCs.push_back(DispBC<nlayer>(left_group, LoadMode::Relative, 'x', 0.0));
+        this->dispBCs.push_back(DispBC<nlayer>(top_group, LoadMode::Relative, 'y', dy));
+        this->dispBCs.push_back(DispBC<nlayer>(bottom_group, LoadMode::Relative, 'y', 0.0));
+    }
+};
+
+template <int nlayer>
+class LoadDogBoneDisp : public LoadStep<nlayer>
+{
+public:
+    LoadDogBoneDisp(double x, double y, double z,
+                    std::vector<Particle<nlayer> *> &group1,
+                    std::vector<Particle<nlayer> *> &group2)
     {
         this->dispBCs.push_back(DispBC<nlayer>(group1, LoadMode::Relative, 'x', x));
         this->dispBCs.push_back(DispBC<nlayer>(group1, LoadMode::Relative, 'y', y));
@@ -28,6 +47,23 @@ public:
         this->dispBCs.push_back(DispBC<nlayer>(group2, LoadMode::Relative, 'x', 0.0));
         this->dispBCs.push_back(DispBC<nlayer>(group2, LoadMode::Relative, 'y', 0.0));
         this->dispBCs.push_back(DispBC<nlayer>(group2, LoadMode::Relative, 'z', 0.0));
+    }
+};
+
+template <int nlayer>
+class LoadCTForce : public LoadStep<nlayer>
+{
+public:
+    LoadCTForce(double fy,
+                std::vector<Particle<nlayer> *> &mid_group,
+                std::vector<Particle<nlayer> *> &top_group,
+                std::vector<Particle<nlayer> *> &bottom_group)
+    {
+        this->dispBCs.push_back(DispBC<nlayer>(mid_group, LoadMode::Relative, 'y', 0));
+        this->dispBCs.push_back(DispBC<nlayer>(top_group, LoadMode::Relative, 'x', 0.0));
+        this->dispBCs.push_back(DispBC<nlayer>(bottom_group, LoadMode::Relative, 'x', 0.0));
+        this->forceBCs.push_back(ForceBC<nlayer>(top_group, LoadMode::Relative, 0.0, fy, 0.0));
+        this->forceBCs.push_back(ForceBC<nlayer>(bottom_group, LoadMode::Relative, 0.0, -fy, 0.0));
     }
 };
 
